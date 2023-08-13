355666d9912408465671d986f5e0168a80000ada
Luis Rodero-Merino <luis.rod@gmail.com>
2023-08-13 19:36:02 +0200

    Implemented Chapter 02 Exercisese 2.3 and 2.4 (curry and uncurry)

--- src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/exercises/gettingstarted/GettingStarted.scala
@@ -128,14 +128,13 @@ object PolymorphicFunctions:
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
   def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean =
-    println(s"Checking array ${as.mkString("[", ", ", "]")}")
     @annotation.tailrec
-    def subIsSorted(i: Int): Boolean =
+    def go(i: Int): Boolean =
       if as.size <= i + 1 then true
       else
         if gt(as(i), as(i+1)) then false
-        else subIsSorted(i+1)
-    subIsSorted(0)
+        else go(i+1)
+    go(0)
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -148,13 +147,13 @@ object PolymorphicFunctions:
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => (b: B) => f(a,b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
