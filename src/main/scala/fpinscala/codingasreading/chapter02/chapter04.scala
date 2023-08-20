125f535c01f6c57826a4af03dace498ec06e7135
Luis Rodero-Merino <luis.rod@gmail.com>
2023-08-20 13:13:35 +0200

    Implemented Chapter 04 Exercises for Either

--- src/main/scala/fpinscala/codingasreading/chapter02/chapter04.scala
+++ src/main/scala/fpinscala/codingasreading/chapter02/chapter04.scala
@@ -1,5 +1,7 @@
 package fpinscala.codingasreading.chapter02
 
+import scala.util.control.NonFatal
+
 
 enum Option[+A]:
   case Some(get: A)
@@ -10,3 +12,11 @@ def mean(as: Seq[Double]): Option[Double] =
   if as.isEmpty then None
   else Some(as.sum / as.size)
 
+enum Either[+E, +A]:
+  case Left(value: E)
+  case Right(value: A)
+
+def catchNonFatal[A, B](f:A => B): A => Either[Throwable, B] = a =>
+  import Either.*
+  try Right(f(a))
+  catch case NonFatal(t) => Left(t)
--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -8,18 +8,31 @@ enum Either[+E,+A]:
   case Left(get: E)
   case Right(get: A)
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match
+    case Right(a) => Right(f(a))
+    case Left(e) => Left(e)
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match
+    case Right(_) => this
+    case Left(_) => b
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      a <- this
+      b <- b
+    } yield f(a, b)
 
 object Either:
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match
+    case h :: tail => f(h).flatMap(b => traverse(tail)(f).map(s => b :: s))
+    case Nil => Right(Nil)
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(identity)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if xs.isEmpty then
@@ -35,8 +48,15 @@ object Either:
     try Right(a)
     catch case NonFatal(t) => Left(t)
 
-  def map2All[E, A, B, C](a: Either[List[E], A], b: Either[List[E], B], f: (A, B) => C): Either[List[E], C] = ???
+  def map2All[E, A, B, C](a: Either[List[E], A], b: Either[List[E], B], f: (A, B) => C): Either[List[E], C] = (a, b) match
+    case (Left(l1), Left(l2)) => Left(l1 ::: l2)
+    case (Left(l), _) => Left(l)
+    case (_ , Left(l)) => Left(l)
+    case (Right(a) , Right(b)) => Right(f(a, b))
 
-  def traverseAll[E, A, B](as: List[A], f: A => Either[List[E], B]): Either[List[E], List[B]] = ???
+  def traverseAll[E, A, B](as: List[A], f: A => Either[List[E], B]): Either[List[E], List[B]] = as match
+    case h :: tail => map2All(f(h), traverseAll(tail, f), (b, bs) => b :: bs)
+    case Nil => Right(Nil)
 
-  def sequenceAll[E, A](as: List[Either[List[E], A]]): Either[List[E], List[A]] = ???
+  def sequenceAll[E, A](as: List[Either[List[E], A]]): Either[List[E], List[A]] =
+    traverseAll(as, identity)
